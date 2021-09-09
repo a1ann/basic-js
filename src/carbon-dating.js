eb53caa243562df7d31b61113f25e7e06d3627c3
a1ann <alanngomel@gmail.com>
2021-09-09 10:55:06 +0300

    feat: complite 1 and 2 task

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,7 +17,11 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-export default function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function dateSample(sampleActivity) {
+  if (Number(sampleActivity) && typeof (sampleActivity) == 'string') {
+    let n = Number(sampleActivity);
+    let k = Math.log(2) / HALF_LIFE_PERIOD;
+    return Math.ceil(Math.log(MODERN_ACTIVITY / n) / k);
+  }
+  else { return false }
 }
--- src/count-cats.js
+++ src/count-cats.js
@@ -14,7 +14,12 @@ import { NotImplementedError } from '../extensions/index.js';
  * ]) => 3`
  *
  */
-export default function countCats(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function countCats(backyard) {
+  let count = 0;
+  for (let str of backyard) {
+    for (let e of str) {
+      if (e === "^^") { count++; }
+    }
+  }
+  return count;
 }
