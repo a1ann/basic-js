6c2a46995d67ffb8e9086db3b47cf0f72a95ce02
a1ann <alanngomel@gmail.com>
2021-09-12 13:32:21 +0300

    feat: comleat 5,6 task

--- src/hanoi-tower.js
+++ src/hanoi-tower.js
@@ -14,7 +14,10 @@ import { NotImplementedError } from '../extensions/index.js';
  * calculateHanoi(9, 4308) => { turns: 511, seconds: 427 }
  *
  */
-export default function calculateHanoi(/* disksNumber, turnsSpeed */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function calculateHanoi(disksNumber, turnsSpeed) {
+  let turns = Math.pow(2, disksNumber) - 1;
+  let perSecond = 3600 / turnsSpeed;
+  let second = perSecond * turns;
+  let result = { 'turns': turns, 'seconds': Math.floor(second) };
+  return result;
 }
--- src/transform-array.js
+++ src/transform-array.js
@@ -13,7 +13,30 @@ import { NotImplementedError } from '../extensions/index.js';
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  * 
  */
-export default function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function transform(arr) {
+  if (Array.isArray(arr) == false) {
+    throw new Error("'arr' parameter must be an instance of the Array!");
+  }
+  let a = Array.from(arr);
+  let result = [];
+  for (let i = 0; i < a.length; i++) {
+    if (typeof (a[i]) == 'string') {
+      if (a[i] == '--discard-next' && i + 1 < a.length) {
+        a[i + 1] = '*';
+      }
+      if (a[i] == '--discard-prev' && a[i - 1] != '*' && i - 1 >= 0) {
+        result.pop();
+      }
+      if (a[i] == '--double-next' && i + 1 < a.length) {
+        result.push(a[i + 1]);
+      }
+      if (a[i] == '--double-prev' && a[i - 1] != '*' && i - 1 >= 0) {
+        result.push(a[i - 1]);
+      }
+    }
+    else {
+      result.push(a[i]);
+    }
+  }
+  return result;
 }
