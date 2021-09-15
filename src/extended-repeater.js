28a69456c35af4d79fd979e58c5a7b626b426d82
a1ann <alanngomel@gmail.com>
2021-09-15 12:14:00 +0300

    feat : comlite extended-repeaters task

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -15,7 +15,51 @@ import { NotImplementedError } from '../extensions/index.js';
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-export default function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function repeater(str, options) {
+  str = String(str);
+  let rep = 1;
+  let sep = '+';
+  let addStr = '';
+  let addRep = 1;
+  let addSep = '|';
+  let result = '';
+  for (let op in options) {
+    if (op == "repeatTimes") {
+      if (Number.isInteger(options[op])) {
+        rep = options[op];
+      }
+    }
+
+    if (op == "separator") {
+      sep = options[op];
+    }
+
+    if (op == "addition") {
+      addStr = String(options[op]);
+    }
+
+    if (op == "additionRepeatTimes") {
+      if (Number.isInteger(options[op])) {
+        addRep = options[op];
+      }
+    }
+
+    if (op == "additionSeparator") {
+      addSep = options[op];
+    }
+  }
+
+  let j = 0;
+  for (let i = 0; i < rep; i++) {
+    result += str;
+    for (let j = 0; j < addRep; j++) {
+      result += addStr;
+      if (j == addRep - 1) { break; }
+      result += addSep;
+    }
+    if (i == rep - 1) { break; }
+    result += sep;
+  }
+
+  return result;
 }
